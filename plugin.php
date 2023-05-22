2603c4499ff35a3acf203e363057fbb333ac3624
Marco Di Bella <mdb@marcodibella.de>
2023-05-22 14:57:47 +0200

    Add shortcode (backport from cm)

--- plugin.php
+++ plugin.php
@@ -38,3 +38,5 @@ define( __NAMESPACE__ . '\PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 
 require_once( PLUGIN_DIR . 'includes/setup.php' );
 require_once( PLUGIN_DIR . 'includes/backend.php' );
+
+require_once( PLUGIN_DIR . 'includes/shortcodes/shortcode-faq.php' );
